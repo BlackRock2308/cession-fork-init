bba50a9aaf4f0a15211fa7ea542053c8b2681f20
diops <dsokhnadiarra@ept.sn>
2022-10-07 12:17:43 +0000

    Export sous format EXCEL

--- src/app/workstation/components/CDMP/dashboard-dg/dashboard-dg.component.ts
+++ src/app/workstation/components/CDMP/dashboard-dg/dashboard-dg.component.ts
@@ -9,9 +9,8 @@ import { DetailsTableauComponent } from './details-tableau/details-tableau.compo
 import { Subscription } from 'rxjs';
 import { AppConfig } from 'src/app/workstation/model/appconfig';
 import { AppConfigService } from 'src/app/workstation/service/appconfigservice';
-import * as XLSX from 'xlsx';
-import jsPDF from 'jspdf'
-import autoTable from 'jspdf-autotable'
+
+import * as FileSaver from 'file-saver';
 
 import 'jspdf-autotable';
 @Component({
@@ -375,18 +374,22 @@ export class DashboardDGComponent implements OnInit {
     
 
     exportexcel(): void {
-        /* pass here the table id */
-        let element = document.getElementById('excel-table');
-        const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+        import("xlsx").then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(this.demandes);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            this.saveAsExcelFile(excelBuffer, "liste_creance");
+        });
 
-        /* generate workbook and add the worksheet */
+    }
 
-        const wb: XLSX.WorkBook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-        /* save to file */
-        XLSX.writeFile(wb, this.fileName);
-
+    saveAsExcelFile(buffer: any, fileName: string): void {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE
+        });
+        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
     }
     visualiserDetails(demande: DemandeAdhesion) {
         this.demande = {...demande};
