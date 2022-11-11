ae806b6bc4e5b8f16dd35fe2fcf22ffcc52374d3
snndiayeos <ndiayeoumarsahaba@ept.sn>
2022-11-11 08:45:07 -0800

    feat:affichage demande cession en fonction de la pme et du satut

--- src/app/workstation/service/demandes_cession/demandes-cession.service.ts
+++ src/app/workstation/service/demandes_cession/demandes-cession.service.ts
@@ -11,6 +11,7 @@ import { StatutEnum } from '../../model/statut-enum';
 export class DemandesCessionService {
   
   
+  
     
     
   private baseUrl = ApiSettings.API_CDMP;
@@ -104,6 +105,13 @@ public addDemandeCession(demandeCession : any ) : Observable<DemandeCession>{
 
     return this.http.get<any>(`${this.baseUrl}/demandecession/bystatut`,{params});
   }
+
+  getDemandesCessionByPmeAndStatut(idPME: any, statut: string): Observable<any>  {
+    const params = new HttpParams()
+    .set('statut',statut)
+    .set('pme',idPME)
+
+  return this.http.get<any>(`${this.baseUrl}/demandecession/byStatutAndPme`,{params});  }
 
   validateAnalyseRisque(id: any) {
     const req = new HttpRequest('PATCH', `${this.baseUrl}/demandecession/${id}/validateAnalyse`,id, {
