246f117c90ed43a289be52f43f601e3ad4224a8c
diops <dsokhnadiarra@ept.sn>
2022-11-15 16:47:11 +0000

    Modification de la valeur de la decote par le dg

--- src/app/workstation/COMPTABLE_CDMP/convention-signer/convention-signer.component.ts
+++ src/app/workstation/COMPTABLE_CDMP/convention-signer/convention-signer.component.ts
@@ -7,6 +7,7 @@ import Swal from 'sweetalert2';
 import { Convention } from '../../model/convention';
 import { Observation } from '../../model/observation';
 import { StatutEnum } from '../../model/statut-enum';
+import { ConventionService } from '../../service/convention/convention.service';
 import { DemandesCessionService } from '../../service/demandes_cession/demandes-cession.service';
 import { ObservationService } from '../../service/observation/observation.service';
 
@@ -29,21 +30,24 @@ export class ConventionSignerComponent implements OnInit {
     public ref: DynamicDialogRef,
     private demandeCessionService : DemandesCessionService,
     private tokenStorage : TokenStorageService,
+    private conventionService : ConventionService,
     private observationService:ObservationService
   ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       convention: ['', Validators.required],
-      codePIN : ['' , Validators.required]
+      codePIN : ['' , Validators.required],
+      decote : ['']
 
     });
 
     this.demandeCessionService.getDemandeObs().subscribe(data => {
       this.demande = data;
+      this.convention = this.demande.conventions[0]
      
 
-      console.log(this.demande,data)
+      console.log(this.convention)
 
     })
   }
@@ -66,9 +70,21 @@ export class ConventionSignerComponent implements OnInit {
  
   private async signerConventionDG() {
 
+    let body = {
+      valeurDecote: this.form.value['decote'],
+    }
+
+
  
     var  idDemande = this.demande.idDemande
     this.codePIN=this.form.value['codePIN']
+
+    if (this.form.value['decote'] !== null) {
+      this.conventionService.updateConvention(body , this.convention.idConvention)
+      .subscribe((response: any) => {
+        console.log(response)}
+  )}
+
 
     await this.demandeCessionService.signerConventionDG(this.codePIN,this.tokenStorage.getUser().idUtilisateur,idDemande).subscribe
     ((response: any) => {
@@ -77,11 +93,11 @@ export class ConventionSignerComponent implements OnInit {
     },
     (error) => {},
     () => {
-      this.observation.utilisateurid = this.tokenStorage.getUser().idUtilisateur;
-      this.observation.statut={}      
-      this.observation.idDemande = idDemande;
-      this.observation.statut.libelle =StatutEnum.conventionSigneeParDG;
-      this.observationService.postObservation(this.observation).subscribe(data => console.log(data))
+      // this.observation.utilisateurid = this.tokenStorage.getUser().idUtilisateur;
+      // this.observation.statut={}      
+      // this.observation.idDemande = idDemande;
+      // this.observation.statut.libelle =StatutEnum.conventionSigneeParDG;
+      // this.observationService.postObservation(this.observation).subscribe(data => console.log(data))
 
       Swal.fire({
         html: "<p style='font-size: large;font-weight: bold;justify-content:center;'>Votre convention a été signée.</p>",
