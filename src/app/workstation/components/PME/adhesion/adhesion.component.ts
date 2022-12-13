56ce36551e01ada62228a8b0ba33a8e7a1d1477a
Fatimetou Diagne <fmdiagne@modelsis.sn>
2022-12-13 15:18:32 +0000

    corrections

--- src/app/workstation/components/PME/adhesion/adhesion.component.ts
+++ src/app/workstation/components/PME/adhesion/adhesion.component.ts
@@ -3,7 +3,6 @@ import {
   FormGroup,
   Validators,
   FormBuilder,
-  AbstractControlOptions,
   ValidationErrors,
   AbstractControl,
 } from "@angular/forms";
@@ -18,7 +17,6 @@ import { PmeService } from "src/app/workstation/service/pme/pmeservice.service";
 import { DemandeAdhesion } from "../../../model/demande";
 import { Observation } from "src/app/workstation/model/observation";
 import { TokenStorageService } from "src/app/auth/token-storage.service";
-import { StatutEnum } from "src/app/workstation/model/statut-enum";
 import { ObservationService } from "src/app/workstation/service/observation/observation.service";
 import { SearchCountryField, CountryISO } from "ngx-intl-tel-input";
 
@@ -47,18 +45,15 @@ export class AdhesionComponent implements OnInit {
   pme: PME;
   myFiles: Document[] = [];
   demande: DemandeAdhesion;
-  minDate:any;
+  minDate: any;
   observation: Observation = {};
   constructor(
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
     private router: Router,
     private adhesionService: AdhesionService,
     private pmeService: PmeService,
-    private uploadFileService: FileUploadService,
-    private tokenStorage: TokenStorageService,
-    private observationService: ObservationService
-  ) {}
+    private uploadFileService: FileUploadService
+  ) { }
 
   ngOnInit(): void {
     this.formeJuridique = [
@@ -123,11 +118,11 @@ export class AdhesionComponent implements OnInit {
       capitalSocial: [''],
       autorisationMinisterielle: ['']
     });
-    
+
   }
 
-  setMinDate(){
-    this.minDate=((new Date(this.form.value['dateCreation'])).getDate()+1).toString()
+  setMinDate() {
+    this.minDate = ((new Date(this.form.value['dateCreation'])).getDate() + 1).toString()
     console.log(this.minDate)
   }
 
@@ -225,8 +220,8 @@ export class AdhesionComponent implements OnInit {
       chiffresDaffaires: this.pme.chiffresDaffaires,
       cniRepresentant: this.pme.cniRepresentant,
       dateImmatriculation: this.pme.dateImmatriculation,
-      telephonePME:this.pme.telephonePME,
-      capitalSocial:this.pme.capitalSocial,
+      telephonePME: this.pme.telephonePME,
+      capitalSocial: this.pme.capitalSocial,
       autorisationMinisterielle: this.pme.autorisationMinisterielle,
       date_soumission: new Date(),
     };
@@ -247,8 +242,8 @@ export class AdhesionComponent implements OnInit {
         .subscribe((response: any) => {
           let data = JSON.parse(JSON.stringify(response));
           if (data && data.idDemande != null) {
-            this.uploadFileService.uploadFile( "/pme/", data.pme.idPME, this.selectedRCCMFiles, "RCCM" ) .subscribe();
-            this.uploadFileService.uploadFile( "/pme/", data.pme.idPME, this.selectedNINEAFiles, "NINEA").subscribe();
+            this.uploadFileService.uploadFile("/pme/", data.pme.idPME, this.selectedRCCMFiles, "RCCM").subscribe();
+            this.uploadFileService.uploadFile("/pme/", data.pme.idPME, this.selectedNINEAFiles, "NINEA").subscribe();
             Swal.fire({
               html: "<p style='font-size: large;font-weight: bold;justify-content:center;'>Votre demande d'adhésion a été prise en compte.</p><br><p style='font-size: large;font-weight: bold;'>Vous allez recevoir un message de la part du service de cession de créances dans les plus brefs délais.</p>",
               color: "#203359",
